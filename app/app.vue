da4f1b2571a8d858604b219594a12150f0115c1e
czarsbenemoriendi <93704910+czarsbenemoriendi@users.noreply.github.com>
2026-09-17 09:12:03 +0200

    fix: replace nuxt favicon with vue wroclaw logo

--- app/app.vue
+++ app/app.vue
@@ -6,7 +6,9 @@ useHead({
     { name: 'viewport', content: 'width=device-width, initial-scale=1' }
   ],
   link: [
-    { rel: 'icon', href: '/favicon.ico' }
+    { rel: 'icon', type: 'image/svg+xml', href: '/vuejs-wroclaw.svg' },
+    { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+    { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }
   ],
   htmlAttrs: {
     lang: 'pl'
